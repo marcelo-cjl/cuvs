1fa7d61e9ce567ecfab5496e34260e786609dedd
Ubuntu <ubuntu@ip-10-15-3-93.us-west-2.compute.internal>
2025-10-23 08:01:45 +0000

    cagra

--- examples/cpp/src/cagra_example.cu
+++ examples/cpp/src/cagra_example.cu
@@ -23,17 +23,19 @@
 
 #include <rmm/mr/device/device_memory_resource.hpp>
 #include <rmm/mr/device/pool_memory_resource.hpp>
+#include <unordered_set>
 
 #include "common.cuh"
 
 void cagra_build_search_simple(raft::device_resources const& dev_resources,
-                               raft::device_matrix_view<const float, int64_t> dataset,
-                               raft::device_matrix_view<const float, int64_t> queries)
+                               raft::device_matrix_view<const float, int64_t> train_set,
+                               raft::device_matrix_view<const float, int64_t> query_set,
+                               raft::device_matrix_view<const int64_t, int64_t> truth_set)
 {
   using namespace cuvs::neighbors;
 
-  int64_t topk      = 12;
-  int64_t n_queries = queries.extent(0);
+  int64_t topk      = 10;
+  int64_t n_queries = query_set.extent(0);
 
   // create output arrays
   auto neighbors = raft::make_device_matrix<uint32_t>(dev_resources, n_queries, topk);
@@ -41,9 +43,11 @@ void cagra_build_search_simple(raft::device_resources const& dev_resources,
 
   // use default index parameters
   cagra::index_params index_params;
+  // Set distance metric to Cosine
+  index_params.metric = cuvs::distance::DistanceType::CosineExpanded;
 
-  std::cout << "Building CAGRA index (search graph)" << std::endl;
-  auto index = cagra::build(dev_resources, index_params, dataset);
+  std::cout << "Building CAGRA index (search graph) with Cosine distance" << std::endl;
+  auto index = cagra::build(dev_resources, index_params, train_set);
 
   std::cout << "CAGRA index has " << index.size() << " vectors" << std::endl;
   std::cout << "CAGRA graph has degree " << index.graph_degree() << ", graph size ["
@@ -52,15 +56,74 @@ void cagra_build_search_simple(raft::device_resources const& dev_resources,
   // use default search parameters
   cagra::search_params search_params;
   // search K nearest neighbors
-  cagra::search(dev_resources, search_params, index, queries, neighbors.view(), distances.view());
+  cagra::search(dev_resources, search_params, index, query_set, neighbors.view(), distances.view());
 
   // The call to cagra::search is asynchronous. Before accessing the data, sync by calling
-  // raft::resource::sync_stream(dev_resources);
+  raft::resource::sync_stream(dev_resources);
 
-  print_results(dev_resources, neighbors.view(), distances.view());
+  // Copy results from device to host for recall calculation
+  auto h_neighbors = raft::make_host_matrix<uint32_t, int64_t>(n_queries, topk);
+  auto h_truth_set = raft::make_host_matrix<int64_t, int64_t>(truth_set.extent(0), truth_set.extent(1));
+  
+  raft::copy(h_neighbors.data_handle(), neighbors.data_handle(), neighbors.size(), raft::resource::get_cuda_stream(dev_resources));
+  raft::copy(h_truth_set.data_handle(), truth_set.data_handle(), truth_set.size(), raft::resource::get_cuda_stream(dev_resources));
+  raft::resource::sync_stream(dev_resources);
+
+  // Calculate recall
+  float recall = 0.0;
+  for (int64_t i = 0; i < n_queries; i++) {
+    std::unordered_set<uint32_t> truth_set_i;
+    std::unordered_set<uint32_t> result_set_i;
+    
+    // Fill truth set
+    for (int64_t j = 0; j < topk; j++) {
+      truth_set_i.insert(static_cast<uint32_t>(h_truth_set(i, j)));
+    }
+    
+    // Fill result set
+    for (int64_t j = 0; j < topk; j++) {
+      result_set_i.insert(h_neighbors(i, j));
+    }
+    
+    // Count common elements
+    int common = 0;
+    for (auto it = result_set_i.begin(); it != result_set_i.end(); ++it) {
+      if (truth_set_i.find(*it) != truth_set_i.end()) {
+        common++;
+      }
+    }
+    recall += static_cast<float>(common) / static_cast<float>(topk);
+  }
+  recall /= n_queries;
+  std::cout << "Recall: " << recall << std::endl;
 }
 
-int main()
+template <typename T, typename idxT>
+raft::device_matrix<T, idxT> read_fbin(raft::device_resources const& dev_resources,
+                                       std::string fname)
+{
+  std::ifstream datafile(fname, std::ifstream::binary);
+  uint32_t N;
+  uint32_t dim;
+  datafile.read((char*)&N, sizeof(uint32_t));
+  datafile.read((char*)&dim, sizeof(uint32_t));
+  std::cout << "Read in file - N:" << N << ", dim:" << dim << std::endl;
+
+  std::vector<T> data;
+  data.resize((size_t)N * (size_t)dim);
+  datafile.read(reinterpret_cast<char*>(data.data()), (size_t)N * (size_t)dim * sizeof(T));
+  datafile.close();
+
+  auto dataset = raft::make_device_matrix<T, idxT>(dev_resources, N, dim);
+  raft::copy(dataset.data_handle(),
+             data.data(),
+             data.size(),
+             raft::resource::get_cuda_stream(dev_resources));
+
+  return dataset;
+}
+
+int main(int argc, char** argv)
 {
   raft::device_resources dev_resources;
 
@@ -69,22 +132,20 @@ int main()
     rmm::mr::get_current_device_resource(), 1024 * 1024 * 1024ull);
   rmm::mr::set_current_device_resource(&pool_mr);
 
-  // Alternatively, one could define a pool allocator for temporary arrays (used within RAFT
-  // algorithms). In that case only the internal arrays would use the pool, any other allocation
-  // uses the default RMM memory resource. Here is how to change the workspace memory resource to
-  // a pool with 2 GiB upper limit.
-  // raft::resource::set_workspace_to_pool_resource(dev_resources, 2 * 1024 * 1024 * 1024ull);
+  std::string train_file = "/home/ubuntu/data/cohere/cohere.fbin";
+  std::string query_file = "/home/ubuntu/data/cohere/cohere_query.fbin";
+  std::string truth_file = "/home/ubuntu/data/cohere/cohere_query_COSINE_0.01_100.truth";
 
-  // Create input arrays.
-  int64_t n_samples = 10000;
-  int64_t n_dim     = 90;
-  int64_t n_queries = 10;
-  auto dataset      = raft::make_device_matrix<float, int64_t>(dev_resources, n_samples, n_dim);
-  auto queries      = raft::make_device_matrix<float, int64_t>(dev_resources, n_queries, n_dim);
-  generate_dataset(dev_resources, dataset.view(), queries.view());
+  auto train_set = read_bin_dataset<float, int64_t>(dev_resources, train_file);
+  auto query_set = read_bin_dataset<float, int64_t>(dev_resources, query_file);
+  auto truth_set = read_bin_dataset<int64_t, int64_t>(dev_resources, truth_file);
 
-  // Simple build and search example.
+  std::cout << "Train set: rows=" << train_set.extent(0) << ", cols=" << train_set.extent(1) << std::endl;
+  std::cout << "Query set: rows=" << query_set.extent(0) << ", cols=" << query_set.extent(1) << std::endl;
+  std::cout << "Truth set: rows=" << truth_set.extent(0) << ", cols=" << truth_set.extent(1) << std::endl;
+
   cagra_build_search_simple(dev_resources,
-                            raft::make_const_mdspan(dataset.view()),
-                            raft::make_const_mdspan(queries.view()));
+                            raft::make_const_mdspan(train_set.view()),
+                            raft::make_const_mdspan(query_set.view()),
+                            raft::make_const_mdspan(truth_set.view()));
 }
